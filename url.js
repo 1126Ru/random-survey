bf983e62ecadbe681b3d911c43fe640669f217cd
1126Ru <141096373+1126Ru@users.noreply.github.com>
2023-08-04 17:00:33 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,14 @@
 
 
 const urls = [
-	'https://www.masterhsiao.com.tw/IRP/IRP.php',
-	'https://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/ccd=bVheFv/login?jstimes=1&loadingjs=1&o=dwebmge&ssoauth=1&cache=1690850843887',
-	'https://www.nstc.gov.tw/folksonomy/list/a4b16d30-0343-420d-93e7-ff2fa0daf62f?l=ch'
+	'https://www.surveycake.com/s/z6VzB',
+	'https://www.surveycake.com/s/gOPaB',
+	'https://www.surveycake.com/s/RyKBn',
+	'https://www.surveycake.com/s/oqDWm',
+	'https://www.surveycake.com/s/v6WqP',
+	'https://www.surveycake.com/s/wgnXo'
+	'https://www.surveycake.com/s/0oV74'
+        'https://www.surveycake.com/s/r2YOd'
+        'https://www.surveycake.com/s/x18mO'
+	'https://www.surveycake.com/s/8yqVl'
 ];
